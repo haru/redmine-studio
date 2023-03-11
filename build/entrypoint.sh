306fe3a3dc4f7c819fe731130ac55df2d7fcc0ad
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2023-03-11 22:28:32 +0900

    change port

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,4 +5,4 @@ bundle exec rake redmine:plugins:migrate
 bundle exec rake db:migrate RAILS_ENV=test
 bundle exec rake redmine:plugins:migrate RAILS_ENV=test
 
-/bin/sh -c "exec ${OPENVSCODE_SERVER_ROOT}/bin/openvscode-server --host 0.0.0.0 --without-connection-token ¥"${@}¥"" --
+/bin/sh -c "exec ${OPENVSCODE_SERVER_ROOT}/bin/openvscode-server --host 0.0.0.0 --port 4000 --without-connection-token ¥"${@}¥"" --
