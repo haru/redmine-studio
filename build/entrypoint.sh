c1513ed9c3788d6acb5f923e9d0611ded3f01595
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2023-03-11 13:37:35 +0900

    test db migration

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 bundle install
 bundle exec rake db:migrate
+bundle exec rake redmine:plugins:migrate
+bundle exec rake db:migrate RAILS_ENV=test
+bundle exec rake redmine:plugins:migrate RAILS_ENV=test
 
 /bin/sh -c "exec ${OPENVSCODE_SERVER_ROOT}/bin/openvscode-server --host 0.0.0.0 --without-connection-token ¥"${@}¥"" --
